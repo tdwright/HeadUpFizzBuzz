6ee3b5da791e0670dd4ab0998950e2b4d75f7e99
tdwright <tom@tdwright.co.uk>
2018-05-13 21:25:11 +0100

    Function works over Service Bus

--- HeadUpFizzBuzz/HeadUpOverQueue.cs
+++ HeadUpFizzBuzz/HeadUpOverQueue.cs
@@ -1,22 +1,17 @@
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using System.Text;
 
 namespace HeadUpFizzBuzzFunction
 {
     public static class HeadUpOverQueue
     {
-        const string ServiceBusConnectionString = "Endpoint=sb://headupfizzbuzz.servicebus.windows.net/;SharedAccessKeyName=client;SharedAccessKey=Emsv8xhhFfHeCwil4EU/kAnlGaUudTDo+h13y/BbKOY=";
-        const string QueueName = "response";
-        private static QueueClient queue;
-
         [FunctionName("HeadUpOverQueue")]
-        public static void Run([ServiceBusTrigger("request", Connection = "Endpoint=sb://headupfizzbuzz.servicebus.windows.net/;SharedAccessKeyName=client;SharedAccessKey=Emsv8xhhFfHeCwil4EU/kAnlGaUudTDo+h13y/BbKOY=")]string number, TraceWriter log)
+        [return: ServiceBus("response", Connection = "connection")]
+        public static string Run([ServiceBusTrigger("requests", Connection = "connection")]string number, TraceWriter log)
         {
             var json = Shared.HandleRequest(number);
-            queue = new QueueClient(ServiceBusConnectionString, QueueName);
-            queue.SendAsync(new Message(Encoding.UTF8.GetBytes(json)));
+            log.Info(json);
+            return json;
         }
     }
 }
--- HeadUpFizzBuzz/HeadUpOverRest.cs
+++ HeadUpFizzBuzz/HeadUpOverRest.cs
@@ -8,17 +8,20 @@ using Newtonsoft.Json;
 
 namespace HeadUpFizzBuzzFunction
 {
-    public static class HttpRest
+    public static class HeadUpOverRest
     {
         [FunctionName("HeadUpOverRest")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
+            // Really awkward way to get the number value from either GET or POST...
             string num = req.Query["number"];
             string reqBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(reqBody);
             num = num ?? data?.number;
 
+            // Process the number and return the JSON
             string json = Shared.HandleRequest(num);
+            log.Info(json);
             return new OkObjectResult(json);
         }
     }
